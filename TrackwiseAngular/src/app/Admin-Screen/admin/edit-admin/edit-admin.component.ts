53fb7532a13c5ebe7df15275212fe8eae9f78478
u20540176 <u20540176@tuks.co.za>
2023-05-20 19:46:17 +0200

    Admin and Client Functionality

--- TrackwiseAngular/src/app/Admin-Screen/admin/edit-admin/edit-admin.component.ts
+++ TrackwiseAngular/src/app/Admin-Screen/admin/edit-admin/edit-admin.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DataService } from 'src/app/services/data.service';
+import { Admin } from 'src/app/shared/admin';
 
 @Component({
   selector: 'app-edit-admin',
   templateUrl: './edit-admin.component.html',
   styleUrls: ['./edit-admin.component.scss']
 })
-export class EditAdminComponent {
+export class EditAdminComponent implements OnInit {
 
+  adminDetails: Admin =
+  {
+    admin_ID:0,
+    name:"",
+    lastname:"",
+    email:"",
+    password:"",
+    
+  };
+
+  constructor(private route: ActivatedRoute, private dataService: DataService, private router:Router) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe({
+      next: (params) => {
+
+          this.dataService.GetAdmin(params['admin_ID']).subscribe({
+            next: (response) => {
+              this.adminDetails = response;
+            }
+          })
+
+      }
+    })
+  }
+
+  EditAdmin()
+  {    
+    this.dataService.EditAdmin(this.adminDetails.admin_ID, this.adminDetails).subscribe({
+      next: (response) => {this.router.navigate(['/Admin-Screen/admin'])}
+    })
+    console.log('yes')
+  }
 }
---
+++ TrackwiseAngular/src/app/shared/admin.ts
@@ -0,0 +1,8 @@
+export interface Admin {
+    admin_ID: number;
+    name: string;
+    lastname:String;
+    email:String;
+    password: String;
+
+}
