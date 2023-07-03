7e98de08f73334637f00cbd6dd67867f04e4096a
u21681458 <u21681458@tuks.co.za>
2023-07-03 10:05:04 +0200

    Added orders page

--- TrackwiseAngular/src/app/Customer-Screen/customer-orders/customer-orders.component.ts
+++ TrackwiseAngular/src/app/Customer-Screen/customer-orders/customer-orders.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
+class Order {
+  constructor(public order_id: number, public date: string, public status: string) {}
+}
+
 @Component({
   selector: 'app-customer-orders',
   templateUrl: './customer-orders.component.html',
   styleUrls: ['./customer-orders.component.scss']
 })
 export class CustomerOrdersComponent {
+  currentOrders: Order[] = [];
+  orderHistory: Order[] = [];
 
+  addOrder(order: Order) {
+    this.currentOrders.push(order);
+  }
+
+  moveOrderToHistory(orderId: number) {
+    const orderIndex = this.currentOrders.findIndex(order => order.order_id === orderId);
+    if (orderIndex !== -1) {
+      const order = this.currentOrders.splice(orderIndex, 1)[0];
+      this.orderHistory.push(order);
+    }
+  }
 }
