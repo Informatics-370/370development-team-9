a1279e54b206b3d500cbf65f46dff242a9e2bbdb
u21681458 <u21681458@tuks.co.za>
2023-07-11 10:56:23 +0200

    updated app component

--- TrackwiseAngular/src/app/Customer-Screen/customer-landing-page/customer-landing-page.component.ts
+++ TrackwiseAngular/src/app/Customer-Screen/customer-landing-page/customer-landing-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTableDataSource } from '@angular/material/table';
 import { DataService } from 'src/app/services/data.service';
@@ -11,19 +11,36 @@ import { Product } from 'src/app/shared/product';
   styleUrls: ['./customer-landing-page.component.scss'],
   
 })
-export class CustomerLandingPageComponent {
+export class CustomerLandingPageComponent implements OnInit{
 
+  productList: any[] = [];
 
   constructor(private dataService: DataService) {}
 
   ngOnInit(): void {
-    this.GetAllProducts(); 
+    this.GetProductList();
   }
 
- products : Product[] = [];
+  GetProductList() {
+    this.dataService.GetProducts().subscribe(
+      (result: any) => {
+        let productList: any[] = result;
+        productList.forEach((product) => {
+          this.productList.push(product);
+          console.log(product);
+        });
+      },
+      (error: any) => {
+        console.error('Error retrieving product list:', error);
+      }
+    );
+  }
 
-GetAllProducts() {
-  this.products=JSON.parse(localStorage.getItem('product')!);  
+  addToCart(products: any) {
+    console.log(products);
+    let cartItems:any=[];
+    cartItems.push(products);
+    localStorage.setItem('cart',JSON.stringify(cartItems));
   }
 
 
