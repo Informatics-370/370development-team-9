7d36ab09fc19c5e6df7083171174a1b286446441
u21489132 <u21489132@tuks.co.za>
2023-07-20 14:47:29 +0200

    updated products controller

--- TrackwiseAPI/Controllers/ProductController.cs
+++ TrackwiseAPI/Controllers/ProductController.cs
@@ -61,7 +61,7 @@ namespace TrackwiseAPI.Controllers
         [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "Admin")]
         public async Task<IActionResult> AddProduct(ProductVM prodvm)
         {
-            var product = new Product { Product_Name = prodvm.Product_Name, Product_Description = prodvm.Product_Description, Product_Price = prodvm.Product_Price, Product_Category_ID = prodvm.Product_Category_ID, Product_Type_ID = prodvm.Product_Type_ID };
+            var product = new Product { Product_Name = prodvm.Product_Name, Product_Description = prodvm.Product_Description, Product_Price = prodvm.Product_Price,Quantity = prodvm.Product_Quantity ,Product_Category_ID = prodvm.Product_Category_ID, Product_Type_ID = prodvm.Product_Type_ID };
 
             try
             {
--- TrackwiseAPI/Models/ViewModels/ProductVM.cs
+++ TrackwiseAPI/Models/ViewModels/ProductVM.cs
@@ -6,6 +6,7 @@
         public string Product_Name { get; set; }
         public string Product_Description { get; set; }
         public double Product_Price { get; set; }
+        public int Product_Quantity { get; set; }
         public string Product_Category_ID { get; set; }
         public string Product_Type_ID { get; set; }
 
