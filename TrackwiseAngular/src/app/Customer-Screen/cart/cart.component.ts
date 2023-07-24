5c3bf8000a41c0bbd062be4c55022d1b2720f9ff
u21489132 <u21489132@tuks.co.za>
2023-07-24 18:19:23 +0200

    added create order functionality

--- TrackwiseAngular/src/app/Customer-Screen/cart/cart.component.ts
+++ TrackwiseAngular/src/app/Customer-Screen/cart/cart.component.ts
@@ -15,29 +15,31 @@ interface CartItem {
   styleUrls: ['./cart.component.scss']
 })
 
-export class CartComponent implements OnInit{
+export class CartComponent implements OnInit {
 
-  products: any=[];
-  cartItems: any=[];
-  total:number = 0;
-  itemsInCart: number = 0;
+  products: any = [];
+  cartItems: any = [];
+  total: number = 0;
+  orderLines: { productId: string; quantity: number; }[] = [];
 
-  constructor(private router: Router, private dataService : DataService) {}
+  constructor(private router: Router, public dataService: DataService) { }
 
   ngOnInit(): void {
     this.CartItems();
     this.dataService.revertToLogin();
   }
-  
+
   calculateTotal(): number {
     this.total = 0;
-    this.itemsInCart = 0;
-
+    if (!sessionStorage.getItem('cartItem')) {
+      this.total = 0;
+    } else {
       this.cartItems = JSON.parse(sessionStorage.getItem('cartItem')!)
       for (const item of this.cartItems) {
         this.total += item.cartQuantity * item.product_Price;
-        this.itemsInCart += item.cartQuantity;
       }
+
+    }
 
     return this.total;
   }
@@ -46,41 +48,71 @@ export class CartComponent implements OnInit{
     this.router.navigate(['/Customer-Screen/customer-products']);
   }
 
-  CartItems(){
-    if(sessionStorage.getItem('cartItem')){
-      this.cartItems = JSON.parse(sessionStorage.getItem('cartItem')!)
+  CartItems() {
+    if (sessionStorage.getItem('cartItem')) {
+      this.cartItems = JSON.parse(sessionStorage.getItem('cartItem')!);
       this.calculateTotal();
+      this.dataService.calculateQuantity();
+
+      // Populate the orderLines array with product details
+      this.orderLines = this.cartItems.map((item: { product_ID: any; cartQuantity: any; }) => ({
+        productId: item.product_ID,
+        quantity: item.cartQuantity
+      }));
     }
   }
 
-    increaseQuantity(item: any) {
-    let AddCartItem = JSON.parse(sessionStorage.getItem('cartItem')!)
-    let res = AddCartItem.find((element: { product_ID: any; }) => element.product_ID == item.product_ID);
-    res.cartQuantity++; // Increment quantity by 1
-    item.cartQuantity++;
-    sessionStorage.setItem('cartItem', JSON.stringify(AddCartItem)); 
-    this.calculateTotal();
+  increaseQuantity(item: any) {
+    if (item.cartQuantity < item.quantity) {
+      let AddCartItem = JSON.parse(sessionStorage.getItem('cartItem')!)
+      let res = AddCartItem.find((element: { product_ID: any; }) => element.product_ID == item.product_ID);
+      res.cartQuantity++; // Increment quantity by 1
+      item.cartQuantity++;
+      sessionStorage.setItem('cartItem', JSON.stringify(AddCartItem));
+      this.calculateTotal();
+      this.dataService.calculateQuantity();
+    }
   }
 
   decreaseQuantity(item: any) {
-    if (item.cartQuantity && item.cartQuantity > 1) {
+    if (item.cartQuantity > 1) {
       let AddCartItem = JSON.parse(sessionStorage.getItem('cartItem')!)
       let res = AddCartItem.find((element: { product_ID: any; }) => element.product_ID == item.product_ID);
       res.cartQuantity--;
-      item.cartQuantity--; 
+      item.cartQuantity--;
       sessionStorage.setItem('cartItem', JSON.stringify(AddCartItem));
       this.calculateTotal();
+      this.dataService.calculateQuantity();
     }
   }
 
   removeFromCart(index: number) {
     let AddCartItem = JSON.parse(sessionStorage.getItem('cartItem')!);
-  
+
     if (index !== -1) {
       this.cartItems.splice(index, 1);
       AddCartItem.splice(index, 1); // Remove the item from the cart
       sessionStorage.setItem('cartItem', JSON.stringify(AddCartItem));
       this.calculateTotal();
+      this.dataService.calculateQuantity();
     }
+  }
+
+  checkout(): void {
+    // Perform any validation or additional logic before calling the service method
+
+    // Call the CreateOrder method in your service and pass the orderLines
+    this.dataService.CreateOrder({ orderLines: this.orderLines }).subscribe(
+      () => {
+        // Handle success or display success message
+        sessionStorage.removeItem('cartItem');
+        this.dataService.calculateQuantity();
+        this.router.navigate(['/Customer-Screen/customer-products']);
+      },
+      (error) => {
+        // Handle error or display error message
+        console.error('Error creating order:', error);
+      }
+    );
   }
 }
--- TrackwiseAngular/src/app/shared/order.ts
+++ TrackwiseAngular/src/app/shared/order.ts
@@ -1,4 +1,34 @@
-export interface Order {
-    productId: string;
-    quantity: number;
+export interface Order{
+
+    order_ID: string;
+    date: Date;
+    total: number;
+    status: string;
+    customer_ID: string;
+    orderLines: 
+        {
+        order_line_ID: string;
+        product: {          
+            product_ID: string;
+            product_Name: string;
+            product_Description: string;
+            product_Price:number;
+            quantity:number;
+            cartQuantity?:number;
+        
+            productType:{
+                product_Type_ID:string;
+                name:string;
+                description:string;
+            };
+        
+            productCategory:{
+                product_Category_ID:string;
+                name:string;
+                description:string;
+            };
+        },
+        quantity: number;
+        subTotal: number;
+    }
 }
