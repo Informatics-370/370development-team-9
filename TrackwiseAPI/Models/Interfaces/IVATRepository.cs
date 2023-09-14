05f7c6099a82668e1dbe7d1ca14825ce2cd090c6
u21489132 <u21489132@tuks.co.za>
2023-09-14 13:52:12 +0200

    added vat table and added update and get methods

---
+++ TrackwiseAPI/Models/Interfaces/IVATRepository.cs
@@ -0,0 +1,11 @@
+﻿using TrackwiseAPI.Models.Entities;
+
+namespace TrackwiseAPI.Models.Interfaces
+{
+    public interface IVATRepository
+    {
+        void Update(VAT vat);
+        Task<bool> SaveChangesAsync();
+        Task<VAT> GetVATAsync();
+    }
+}
