a6cb1350accba30294ece9a608f7df0937197bff
u21489132 <u21489132@tuks.co.za>
2023-09-29 18:31:28 +0200

    added timer for 2 factor

--- TrackwiseAngular/src/app/Authentication/two-factor-auth/two-factor-auth.component.ts
+++ TrackwiseAngular/src/app/Authentication/two-factor-auth/two-factor-auth.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
@@ -10,10 +10,10 @@ import { TwoFactor } from 'src/app/shared/twoFactor';
   templateUrl: './two-factor-auth.component.html',
   styleUrls: ['./two-factor-auth.component.scss']
 })
-export class TwoFactorAuthComponent {
+export class TwoFactorAuthComponent implements OnInit, OnDestroy{
 
  twoFactorFormGroup: FormGroup = this.fb.group({
-    username: JSON.parse(localStorage.getItem("User")!),
+    username: JSON.parse(sessionStorage.getItem("User")!),
     code: ['', Validators.required],
   })
 
@@ -25,14 +25,84 @@ export class TwoFactorAuthComponent {
 
   isLoading:boolean = false
   errorMessage: string = '';
+  remainingTime: number = 0; // Initialize remaining time
+  timerInterval: any;
+  expirationTime!: Date;
+  expireTime: any;
 
   constructor(private router: Router, private dataService: DataService, private fb: FormBuilder, private snackBar: MatSnackBar) { }
 
+
   ngOnInit(): void {
+    this.remainingTime = 0;
+    // Retrieve the expiration time from sessionStorage
+    const storedExpirationTime = new Date(JSON.parse(sessionStorage.getItem('OTPtime')!)).getTime();
+
+    // Get the current time
+    const currentTime = new Date().getTime();
+
+    // Calculate the remaining time in milliseconds
+    this.remainingTime = Math.floor((storedExpirationTime - currentTime) / 1000);
+    if(this.remainingTime <= 0)
+    {
+      this.remainingTime = 0
+    }
+    this.startTimer(); // Start the timer when the component initializes
+  }
+
+  ngOnDestroy(): void {
+    this.stopTimer(); // Stop the timer when the component is destroyed
+  }
+
+  startTimer() {
+    this.timerInterval = setInterval(() => {
+      if (this.remainingTime > 0) {
+        this.remainingTime--;
+      } else {
+        this.stopTimer();
+        // Handle timer expiration (e.g., show a message, allow resending OTP).
+        this.errorMessage = 'Time has expired. Please resend OTP if needed.';
+      }
+    }, 1000); // Update every 1 second
+  }
+
+  stopTimer() {
+    clearInterval(this.timerInterval);
+  }
+
+  formatTime(seconds: number): string {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    return `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
+  }
+
+  async ResendTwoFactor() {
+    this.stopTimer()
+    await this.dataService.ResendTwoFactor(this.twoFactorFormGroup.value.username).subscribe(
+      (result) => {
+        const user = result.token.value.user;
+        sessionStorage.setItem('User', JSON.stringify(user));
+        sessionStorage.setItem('OTPtime', JSON.stringify(result.expireOTPtime));
+
+        this.remainingTime = 0;
+        // Retrieve the expiration time from sessionStorage
+        const storedExpirationTime = new Date(JSON.parse(sessionStorage.getItem('OTPtime')!)).getTime();
+    
+        // Get the current time
+        const currentTime = new Date().getTime();
+    
+        // Calculate the remaining time in milliseconds
+        this.remainingTime = Math.floor((storedExpirationTime - currentTime) / 1000);
+        if(this.remainingTime <= 0)
+        {
+          this.remainingTime = 0
+        }
+        this.startTimer(); // Start the timer when the component initializes
+      })
+      
   }
 
   TwoFactorAuth() {
-    const role = JSON.parse(localStorage.getItem("Role")!);
   
     // Create an observable for the ConfirmTwoFactor call
     const confirmTwoFactor$ = this.dataService.ConfirmTwoFactor(this.twoFactorFormGroup.value);
@@ -42,10 +112,12 @@ export class TwoFactorAuthComponent {
       (result) => {
         sessionStorage.setItem('User', JSON.stringify(result.token.value.user));
         sessionStorage.setItem('Token', result.token.value.token);
+        let role = result.role;
         sessionStorage.setItem('Role', JSON.stringify(role));
         this.twoFactorFormGroup.reset();
-        localStorage.removeItem("User");
-        localStorage.removeItem('Role');
+
+        sessionStorage.removeItem('OTPtime');
+
   
         if (role == "Admin") {
           this.router.navigateByUrl('Admin-Screen/admin-home');
